6d3a8118f75d0e167ef7c8682a744cc5b5dc360d
CherryJell <rodionklevlin61@gmail.com>
2026-09-14 18:31:26 +0400

    fix: file receiving errors

--- lab_1/src/get_data.py
+++ lab_1/src/get_data.py
@@ -3,20 +3,25 @@
 import argparse
 import pathlib
 
+
 def get_data() -> str:
     """
     Получает путь к файлу из аргументов командной строки.
     Записывает данные из файла в переменную.
-    :return: данные считанные из файла.
+
+    :return text: данные, считанные из файла.
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--data-path", type=pathlib.Path)
+    parser.add_argument("--data-path", type=pathlib.Path,
+                        required=True, help="Путь к файлу с данными")
     args = parser.parse_args()
 
     if not args.data_path.exists():
-        raise SystemExit(f"Входные данные не найдены: {args.data_path} не содержит источник.")
+        raise SystemExit(f"Входные данные не найдены: файл {args.data_path} не существует.")
+
+    if not args.data_path.is_file():
+        raise SystemExit(f"Указанный путь не является файлом: {args.data_path}")
 
     with args.data_path.open(mode="r", encoding="utf-8") as file:
         text = file.read()
-
     return text
